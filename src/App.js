ee425615445089cde4f9641aeae56f7c9eb8ca6b
TWRphantom <54685570+TWRphantom@users.noreply.github.com>
2020-04-07 23:01:34 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,6 @@ import React from 'react';
 import './App.css';
 import Fetcher from './Fetcher';
 import Hooks from './Hooks';
-import Skeleton from '@material-ui/lab/Skeleton';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-  root: {
-    width: 300,
-  },
-});
-
-export default function Animations() {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <Skeleton />
-      <Skeleton animation={false} />
-      <Skeleton animation="wave" />
-    </div>
-  );
-}
 
 function App() {
   return (
