89252df2fb4568f51510ca7051590151f147f7a2
TWRphantom <54685570+TWRphantom@users.noreply.github.com>
2020-04-12 01:18:20 +0000

    fukGoBack

--- src/components/Fetcher.js
+++ src/components/Fetcher.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Skeleton from '@material-ui/lab/Skeleton';
-import Rating from '@material-ui/lab/Rating';
 import { Paper, Button, Grid } from '@material-ui/core';
 
 export default function Fetcher(props) {
@@ -46,17 +45,42 @@ export default function Fetcher(props) {
 
 
     return (
-        <Rating
-            name="hover-feedback"
-            value={value}
-            precision={0.5}
-            onChange={(event, newValue) => {
-                setValue(newValue);
-            }}
-            onChangeActive={(event, newHover) => {
-                setHover(newHover);
-            }}
-        />
-        {value !== null && <Box ml={2}>{labels[hover !== -1 ? hover : value]}</Box>}
+        <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={2}
+        >
+          <Grid item xs>
+            <Typography variant="h6">submits: {clicks}</Typography>
+          </Grid>
+            <Grid item xs>
+              <TextField 
+              label="Type something"
+              helperText="This will be echo echo by the server"
+              value={inputValue} onChange={handleChange} inputProps={{ 'aria-label': 'description' }} />
+            </Grid>
+            <Grid item xs>
+              <Button onClick={doSomething} variant="contained" color="primary" data-something="submit">
+                  submit</Button>
+            </Grid>
+            <Grid item xs>
+              <Paper elevation={1} style={
+                {height:200, width:200, wordBreak: "break-all", padding:4}
+              } >
+                {response?JSON.stringify(response):
+                (<React.Fragment>
+                <Skeleton variant="text" />
+                <Skeleton variant="circle" width={40} height={40} />
+                <Skeleton variant="rect" width={200} height={118} />
+                <Skeleton animation = {false} />
+                <Skeleton animation = "wave" />
+                </React.Fragment>)}
+                </Paper>
+              </Grid>
+        </Grid>
+
     );
 }
+
